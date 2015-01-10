186f89ef5c0e84d2628e8410b9dedbdfebe6e465
KaLeonis <kaleonis@gmail.com>
2015-01-10 18:27:30 +0300

    x

--- src/ru/mogom/Kaleoins_testServlet.java
+++ src/ru/mogom/Kaleoins_testServlet.java
@@ -2,6 +2,11 @@ package ru.mogom;
 
 import java.io.IOException;
 import javax.servlet.http.*;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Entity;
 
 @SuppressWarnings("serial")
 public class Kaleoins_testServlet extends HttpServlet {
@@ -10,5 +15,19 @@ public class Kaleoins_testServlet extends HttpServlet {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 		resp.getWriter().println("Hello, Roman");
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query q = new Query("slider");
+		PreparedQuery pq = datastore.prepare(q);
+		
+		for (Entity e : pq.asIterable()) {
+			Long num = (Long)e.getProperty("num"); 
+			String url = (String)e.getProperty("url"); 
+			String name = (String)e.getProperty("name");
+			resp.getWriter().println(num);
+			resp.getWriter().println(url);
+			resp.getWriter().println(name);
+			resp.getWriter().println("");
+		}
 	}
 }
